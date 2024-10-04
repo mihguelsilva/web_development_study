9761f3da9ddcecbd6362e3b7056fb3d244e7e947
mihguelsilva <mihguelaraujo@gmail.com>
2024-10-03 23:13:23 -0300

    Finishing table's page HTML.

--- js/default.js
+++ js/default.js
@@ -1,18 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-/*    let asideSummary = document.querySelector("aside#summary");                                                                                     
-    let h2Title = document.querySelectorAll("h2.title");                                                                                            
-    h2Title.forEach(function(element, index) {                                                                                                      
-        asideSummary.innerHTML += `<a class='summary' href='#${element.id}' style='display:block;color:white;font-size:14pt;font-family:Arial;cursor:pointer;text-decoration:none;border:1px solid rgba(255,255,255,.1);padding:5px;'>${element.getAttribute('name')}</a>`;                                                                                      
-    });                                                                                                                                             
-    let aSummary = document.querySelectorAll("a.summary");                                                                                          
-    aSummary.forEach(function(element, index) {                                                                                                     
-        element.addEventListener("mouseover", function(e){                                                                                          
-            e.target.style.backgroundColor = "rgba(255,255,255,.1)";                                                                                
-        });                                                                                                                                         
-        element.addEventListener("mouseout", function(e){
-	    e.target.style.backgroundColor = "rgba(1,1,1,.0)";
-	});
-    });*/
     let buttonExample = document.querySelectorAll("button.example");
     buttonExample.forEach(function(element, index) {
 	element.addEventListener("mouseover", function(e) {
