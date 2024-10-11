67c584c2a7e0a6abfc6f63df30a7ca429e286633
mihguelsilva <mihguelaraujo@gmail.com>
2024-10-11 01:36:35 -0300

    Enhancing HTML, CSS and JS pages while studying PHP

---
+++ pages/module_03/01_introduction_php.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang='pt-br'>
+    <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Class 1</title>
+	<link rel="stylesheet" href="/styles/index.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Black+Han+Sans&display=swap" rel="stylesheet">
+	<script src="/js/index.js"></script>
+    </head>
+    <body>
+	<header title="Introduction to PHP, A better view of PHP" id="header">
+	</header>
+	<main>
+	    <section class="content" id="class">
+		<figure id="php">
+		    <img src="/img/php-logo.png" alt="php-logo" width="100%">
+		</figure>
+		<article>
+		<h2 class="content">Login Page</h2>
+		<p class="content">
+		    To create a login page it's needful to use session.
+		</p>
+		<h2 class="content">Why use PHP?</h2>
+		<p class="content">
+		    When PHP it's not used, all sites becomes simple and limited. Using PHP, we can
+		    create virtual stores, including new's site, where we need PHP and database, as
+		    MySQL, to store those news and replicate into the site.
+		</p>
+		</article>
+	    </section>
+	</main>
+	<footer>
+	</footer>
+    </body>
+</html> 
